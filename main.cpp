ff250324f9ccf05f22cda417506bb14d12ef6a5f
Dan-Ant <daniele.antonacci@marconibari.edu.it>
2024-11-14 09:30:59 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,27 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+float p, b, a;
+cout << "La giostra parte solo quando presenta 30 persone su di essa" << endl;
+p=0;
+while (30>p)
+{
+    cout << "Quanti adulti salgono?" << endl;
+    cin >> a;
+    cout << "Quanti bambini salgono?" << endl;
+    cin >> b;
+    p = (b/2+a)+p;
+    if (p=29.5)
+    {
+        p=p+0.5;
+    }
+}
+if (p>30)
+{
+    cout << "Devono scendere " << p-30 << " persone" << endl;
+
+}
+cout << "Sono salite " << p << " persone la giostra può partire" << endl;
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
